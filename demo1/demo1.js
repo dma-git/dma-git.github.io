feb80e7da68e9faaeaf8434cbf1f12ce02adebb5
rubaiyat <github@factorynoir.com>
2021-04-07 18:59:53 -0400

    added demo1

---
+++ demo1/demo1.js
@@ -0,0 +1,25 @@
+function setup() { createCanvas(600, 600);
+}
+function draw() {
+background(1, 33, 7);
+fill(156, 108, 64);
+circle(300, 300, 300);
+line(330, 400, 300, 420);
+line(270, 400, 300, 420); 
+fill(300, 300, 300);
+circle(225, 265, 50);
+circle(375, 265, 50);
+fill (69, 42, 0);
+circle(225, 265, 40);
+circle(375, 265, 40);
+fill(0, 0, 0);
+circle(225, 265, 30);
+circle(375, 265, 30);
+fill(300, 300, 300);
+circle(235, 255, 10);
+circle(385, 255, 10);
+circle(215, 275, 5);
+circle(365, 275, 5);
+fill(156, 108, 64);
+triangle(280, 310, 300, 330, 320, 310);
+}

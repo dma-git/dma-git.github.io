2f86cca37101b5ca1a573ab89473e75b5b323922
rubaiyat <gitub@factorynoir.com>
2025-10-27 14:30:30 -0700

    add p5 selfie

---
+++ selfie1/selfie1.js
@@ -0,0 +1,25 @@
+function setup() {
+createCanvas(600, 600);
+}
+function draw() {
+background(204);
+fill(230, 190, 185);
+ellipse(280, 300, 200, 280);
+
+//left eye
+fill(220, 220, 225);
+ellipse(240, 260, 40, 25);
+  fill(51);
+ellipse(240, 260, 15, 15);
+fill(255);
+
+
+//right eye
+fill(220, 220, 225);
+ellipse(320, 260, 40, 25);
+  fill(51);
+ellipse(320, 260, 15, 15);
+
+
+
+}
